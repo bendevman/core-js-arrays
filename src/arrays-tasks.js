fda31e1495c31b15b8533cb6a77ea6d12a4b2be8
BenDevman <bendevman@gmail.com>
2025-01-11 14:40:06 +0300

    feat: getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -420,8 +420,10 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .filter((number) => number % 2)
+    .map((number) => numbers.indexOf(number));
 }
 
 /**
