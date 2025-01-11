4543e95d8c59a79a6f4b149e1e0b4bdcd7d79ce9
BenDevman <bendevman@gmail.com>
2025-01-11 13:05:59 +0300

    feat: sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,13 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const bigestArray = arr1.length >= arr2.length ? arr1 : arr2;
+  return bigestArray.map((_, i) => {
+    const arr1Item = arr1[i] ? arr1[i] : 0;
+    const arr2Item = arr2[i] ? arr2[i] : 0;
+    return arr1Item + arr2Item;
+  });
 }
 
 /**
